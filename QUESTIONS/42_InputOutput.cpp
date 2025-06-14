540f112d31609a41508e26deb86d19ecdb571a1a
Chhavi <gusainchhavi@gmail.com>
2025-06-14 22:04:20 +0530

    add file

---
+++ QUESTIONS/42_InputOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Function to read and print the number
+void printNumber() {
+    int num;
+    cin >> num;         // Taking input from user
+    cout << num;        // Printing the input
+}
+
+int main() {
+    printNumber();       // Calling the function
+    return 0;
+}
+
