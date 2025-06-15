2bcae02d8084a896dc525953ecc8f1d75c2cbcf4
Chhavi <gusainchhavi@gmail.com>
2025-06-15 13:42:55 +0530

    add file

---
+++ NOTES/22_IfElseStatements.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int age = 10;
+    
+    if (age >= 18) cout << "You are an adult" << "\n";
+    else cout << "You are not an adult" << "\n";
+    
+    return 0;
+}
